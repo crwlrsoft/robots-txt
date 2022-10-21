097dd035851f7db9c86731d4078ba0f1437b633e
enricodias <enricodias@gmail.com>
2022-10-21 19:46:06 -0300

    Refactor tests to work with data providers

--- tests/EncodingTest.php
+++ tests/EncodingTest.php
@@ -7,23 +7,21 @@ use PHPUnit\Framework\TestCase;
 
 final class EncodingTest extends TestCase
 {
-    public function test_decodes_unnecessarily_encoded_characters(): void
+    /**
+     * @return Generator&iterable<string[]> [$expected, $path]
+     */
+    public function pathProvider(): Generator
     {
-        $this->assertEquals('/foo/bar', Encoding::decodePercentEncodedAsciiCharactersInPath('/f%6F%6f/b%61r'));
+        yield 'Unnecessarily encoded characters' => ['/foo/bar',       '/f%6F%6f/b%61r'];
+        yield 'Non ASCII characters'             => ['/f%F6%F6/b%E4r', '/f%F6%F6/b%E4r'];
+        yield 'Encoded reserved characters'      => ['%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D', '%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D'];
     }
 
-    public function test_not_decodes_non_ascii_characters(): void
+    /**
+     * @dataProvider pathProvider
+     */
+    public function test_decode(string $expected, string $path): void
     {
-        $this->assertEquals('/f%F6%F6/b%E4r', Encoding::decodePercentEncodedAsciiCharactersInPath('/f%F6%F6/b%E4r'));
-    }
-
-    public function test_not_decodes_encoded_reserved_characters(): void
-    {
-        $this->assertEquals(
-            '%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D',
-            Encoding::decodePercentEncodedAsciiCharactersInPath(
-                '%3A%2F%3F%23%5B%5D%40%21%24%26%27%28%29%2A%2B%2C%3B%3D'
-            )
-        );
+        $this->assertEquals($expected, Encoding::decodePercentEncodedAsciiCharactersInPath($path));
     }
 }
