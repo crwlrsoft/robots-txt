60eb38ea22e1210e7c0d6f13d3f387a78aeeb8d7
otsch <otsch@users.noreply.github.com>
2025-11-21 15:26:11 +0100

    Add PHP 8.5 to github CI

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -14,6 +14,7 @@ return (new Config())
         'strict_param' => true,
         'array_syntax' => ['syntax' => 'short'],
         'no_unused_imports' => true,
+        'operator_linebreak' => ['only_booleans' => true, 'position' => 'end'],
     ])
     ->setRiskyAllowed(true)
     ->setUsingCache(true);
